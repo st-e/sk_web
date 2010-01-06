b2615414c46f73204d9bbf09a86fb1e88268ca2e
Martin Herrmann <martin@famherrmann.de>
2010-01-06 16:01:03 +0000

    Added human_attribute_override plugin Used human_attribute_override plugin for User
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/trunk/sk_web@243 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,18 @@ class User < ActiveRecord::Base
 	set_table_name "user" 
 	set_primary_key "username"
 
-	HUMANIZED_ATTRIBUTES = { :current_password => "Altes Passwort" }
+	# Associations
+	# It's called associated_person rather than person because the attribute
+	# name should be different from the foreign key. Otherwise, we could not
+	# distinguish between accesses to the attribute and to the key.
+	# TODO rename to the_person, consistent with Flight
+	belongs_to :associated_person, :class_name => "Person", :foreign_key => 'person'
 
-	def self.human_attribute_name(attr)
-		HUMANIZED_ATTRIBUTES[attr.to_sym] || super
-	end
-
+	# Additional (non-database) fields
 	attr_accessor :current_password
 
 	# Default validations
-	validates_presence_of   :username # length will also count blanks
+	validates_presence_of   :username,                                :message   => "darf nicht leer sein" # length will also count blanks
 	validates_uniqueness_of :username, :case_sensitive => false,      :message   => "existiert schon"
 	validates_length_of     :username, :minimum => 2,                 :too_short => "muss mindestens {{count}} Zeichen lang sein"
 	validates_format_of     :username, :with => /^[a-zA-Z0-9_.-]*$/,  :message   => "Der Benutzername darf nur Buchstaben, Ziffern, _, . und - enthalten"
@@ -23,21 +25,22 @@ class User < ActiveRecord::Base
 	# way, we can have an edit form without password fields, but if there is a
 	# password field (e. g. on the change password page), it must be specified.
 	# In any case, if a password is given, it must match the confirmation.
-	validates_presence_of     :password, :on => :create
-	validates_presence_of     :password, :allow_nil => true
-	validates_confirmation_of :password
+	validates_presence_of     :password, :on => :create,     :message => "darf nicht leer sein"
+	validates_presence_of     :password, :allow_nil => true, :message => "darf nicht leer sein"
+	validates_confirmation_of :password,                     :message => "stimmt nicht mit Bestätigung überein"
 
-	# It's called associated_person rather than person because the attribute
-	# name should be different from the foreign key. Otherwise, we could not
-	# distinguish between accesses to the attribute and to the key.
-	# TODO rename to the_person, consistent with Flight
-	belongs_to :associated_person, :class_name => "Person", :foreign_key => 'person'
+	# Human names for attributes
+	attr_human_name :username         => 'Benutzername'
+	attr_human_name :current_password => 'Altes Passwort'
+	attr_human_name :password         => 'Passwort'
 
+	# Callbacks
 	# If a password confirmation is given, a password is also given and must be
 	# hashed before saving. If no password confirmation is given, no password
 	# was given either, and the password attribute was not changed. In this
 	# case, it contains the hash and will not be hashed.
 	before_save :hash_password!, :if => :password_confirmation_given?
+
 
 	def self.authenticate(username, password)
 		User.exists? 'username'=>username, 'password'=>mysql_password_hash(password)
