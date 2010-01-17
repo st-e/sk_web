6da694ea24020d97e96a223a231ee6d8850f4080
Martin Herrmann <martin@famherrmann.de>
2010-01-17 18:01:08 +0000

    Automatic heading level management (heading method) Cleaned up headings
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/trunk/sk_web@278 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,7 +55,10 @@ module ApplicationHelper
 		content_for :title do
 			title
 		end
-		"<h2 class=\"page_title\">#{title}</h2>"
+
+		heading title, :class=>"page_title" do
+			yield if block_given?
+		end
 	end
 
 
@@ -159,6 +162,47 @@ module ApplicationHelper
 		}
 
 		out
+	end
+
+	# Creates a heading (<h?>...</h?>) tag at the current level. The current
+	# level is stored in the variable @heading_level. A heading level of x
+	# means that the next heading tag will be <hx>.
+	# This method can be used in two ways:
+	# 1. without block - generates a heading at the current level:
+	#   <%= heading h("Title") %>
+	# 2. with block:
+	#   <% heading h("Title") do %>
+	#     <% heading h("Subtitle 1") do %>
+	#       <div>Contents</div>
+	#     <% end %>
+	#     <% heading h("Subtitle 2") do %>
+	#       <div>Contents</div>
+	#     <% end %>
+	#   <% end %>
+	# The contents will not be escaped.
+	# In any case, options can be given, for example:
+	#   heading "Title", :class=>"page_title"
+	def heading(contents, options=nil)
+		# The first usable heading level (h1 is taken by the application
+		# layout, which does not use this method because it is rendered *after*
+		# the page).
+		@heading_level=2 if !defined? @heading_level
+
+		heading_tag=content_tag "h#{@heading_level}", contents, options
+
+		if block_given?
+			# Block mode, concat the heading yield with the new heading level, return nil
+			concat heading_tag
+
+			@heading_level += 1
+			yield
+			@heading_level -= 1
+
+			nil
+		else
+			# No-block mode, return the heading
+			heading_tag
+		end
 	end
 end
 
