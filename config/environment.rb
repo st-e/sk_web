cffaa2f51c4216b7f79f500556af187590f640f8
Martin Herrmann <martin@famherrmann.de>
2010-07-09 22:40:47 +0000

    Autocreate database.yml from database.yml.dist
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/trunk/sk_web@740 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- config/environment.rb
+++ config/environment.rb
@@ -1,19 +1,27 @@
 # Be sure to restart your server when you modify this file
 
-local_environment=File.join(File.dirname(__FILE__), 'local_environment.rb')
-if File.exist? local_environment
-	require local_environment
-end
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '>=2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
-# Don't enable this, it adds extra output to script/runner invocations. Modify
-# script/server or script/console instead if required.
-#puts "Starting up in #{RAILS_ENV} mode"
+local_environment=File.join(File.dirname(__FILE__), 'local_environment.rb')
+if File.exist? local_environment
+	require local_environment
+end
+
+
+# If the database configuration file does not exist, create it. This is
+# required so we can run rake gems:install even if we haven't configured the
+# database yet.
+# TODO settings.rb has the same thing. Make a function and move here.
+# Also for local main page and local environment
+DatabaseConfigFile=Rails.root.join('config', 'database.yml').to_s
+if (!File.exist? DatabaseConfigFile)
+	# This may run as root (sudo rake gems:install), so preserve
+	FileUtils.cp DatabaseConfigFile+".dist", DatabaseConfigFile, :preserve=>true
+end
 
 Rails::Initializer.run do |config|
 	# Some paths may be set from the environment:
