51ded50695f38e86bb3ab78efa36ff59fdc91395
Martin Herrmann <martin@famherrmann.de>
2010-07-12 15:33:22 +0000

    Added SystemInfo, removed all system calls from views
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/trunk/sk_web@763 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

---
+++ lib/system_info.rb
@@ -0,0 +1,37 @@
+class SystemInfo
+	@data={}
+
+	def self.username
+		@data['username'] ||= shell('whoami')
+		# id -u -n
+	end
+
+	def self.groupname
+		@data['groupname'] ||= shell('id -g -n')
+	end
+
+	def self.hostname
+		@data['hostname'] ||= shell('hostname')
+	end
+
+	def self.id
+		@data['id'] ||= shell('id')
+	end
+
+	def self.groupnames
+		@data['groupnames'] ||= shell('id -G -n')
+	end
+
+	private
+
+	def self.shell(cmd)
+		result=`#{cmd}`
+
+		return nil if !result
+		return nil if $?!=0
+		result.chomp
+	end
+
+
+end
+
